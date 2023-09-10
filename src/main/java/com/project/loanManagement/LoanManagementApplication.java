52137ed0e0ced210abb466e905a45a00254fecd1
zireal23 <skasif232000@gmail.com>
2023-09-10 11:45:24 +0530

    first commit

---
+++ src/main/java/com/project/loanManagement/LoanManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.loanManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/loanManagement/LoanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.loanManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
